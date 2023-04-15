80593c9463978eab8be62de91a6bf309fda67bc1
mtgto <hogerappa@gmail.com>
2023-04-15 14:24:07 +0900

    Add config for wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("Cica"),
+  use_ime = true,
+  font_size = 14.0,
+  color_scheme = "Monokai Remastered",
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  initial_cols = 150,
+  initial_rows = 40,
+}
