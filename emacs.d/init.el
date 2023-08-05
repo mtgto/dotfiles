2ecf661be5823f86e54bd6afda734869eb615d6a
mtgto <hogerappa@gmail.com>
2023-08-05 18:38:05 +0900

    [emacs] ddskk

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,56 +22,17 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
-;; Now you can use leaf!
-(leaf leaf-tree :ensure t)
-(leaf leaf-convert :ensure t)
-(leaf transient-dwim
+;; 個人設定
+(setq auto-save-default nil)
+
+;; <ddskk>
+(leaf ddskk
   :ensure t
-  :bind (("M-=" . transient-dwim-dispatch)))
-
-;; You can also configure builtin package via leaf!
-(leaf cus-start
-  :doc "define customization properties of builtins"
-  :tag "builtin" "internal"
-  :custom ((user-full-name . "Naoya Yamashita")
-           (user-mail-address . "conao3@gmail.com")
-           (user-login-name . "conao3")
-           (truncate-lines . t)
-           (menu-bar-mode . t)
-           (tool-bar-mode . nil)
-           (scroll-bar-mode . nil)
-           (indent-tabs-mode . nil)))
-
-(leaf autorevert
-  :doc "revert buffers when files on disk change"
-  :tag "builtin"
-  :custom ((auto-revert-interval . 0.1))
-  :global-minor-mode global-auto-revert-mode)
-
-;; Nest package configurations
-(leaf flycheck
-  :doc "On-the-fly syntax checking"
-  :emacs>= 24.3
-  :ensure t
-  :bind (("M-n" . flycheck-next-error)
-         ("M-p" . flycheck-previous-error))
-  :custom ((flycheck-emacs-lisp-initialize-packages . t))
-  :hook (emacs-lisp-mode-hook lisp-interaction-mode-hook)
-  :config
-  (leaf flycheck-package
-    :doc "A Flycheck checker for elisp package authors"
-    :ensure t
-    :config
-    (flycheck-package-setup))
-
-  (leaf flycheck-elsa
-    :doc "Flycheck for Elsa."
-    :emacs>= 25
-    :ensure t
-    :config
-    (flycheck-elsa-setup))
-
-  ;; ...
-  )
-
-;; ...
+  :bind
+    (("C-x C-j" . skk-mode)
+     ("C-x j"   . skk-auto-fill-mode))
+  :custom
+  (skk-japanese-message-and-error . t)
+  (skk-show-japanese-menu . t))
+(setq default-input-method "japanese-skk")
+;; </ddskk>
